0ad92843dc0d02c7da9ae3daba3faaa424e06740
Pavel Karpenko <pashakarpenko24@gmail.com>
2022-07-13 21:43:24 +0300

    Homework django week 4

---
+++ accounts/views.py
@@ -0,0 +1,36 @@
+from .forms import UserCreateForm
+from django.urls import reverse_lazy, reverse
+from django.views.generic import FormView
+from django.views import View
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import redirect
+
+
+class RegisterFormView(FormView):
+    form_class = UserCreateForm
+    success_url = reverse_lazy("all_books")
+    template_name = "registration/register.html"
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+
+class LoginFormView(FormView):
+    form_class = AuthenticationForm
+    template_name = "registration/login.html"
+    success_url = reverse_lazy("all_books")
+
+    def form_valid(self, form):
+        self.user = form.get_user()
+        login(self.request, self.user)
+        return super().form_valid(form)
+
+
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return redirect(reverse('all_books'))
+
+
